e101720fa138ef6c7fb171dd508e6f170363d681
timomenceyra <tmenceyra@udesa.edu.ar>
2025-03-28 18:16:45 -0300

    Revisar opcodes

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -4,48 +4,84 @@
 #include "shell.h"
 
 // OPERACIONES ARITMÉTICAS
-#define OPCODE_ADD      0x458   // ADD (register)
-#define OPCODE_ADDI     0x488   // ADD (immediate)
-#define OPCODE_ADDS     0x588   // ADDS (register) REVISAR OPCODE
-#define OPCODE_ADDIS    0xB1   // ADDS (immediate)
-
+#define OPCODE_HLT      0x6A2  // HLT
+#define OPCODE_ADDS     0x558  // ADDS (register)
+#define OPCODE_ADDIS_0  0x588  // ADDS (immediate). SHIFT=000
+#define OPCODE_ADDIS_1  0x58A  // ADDS (immediate). SHIFT=010
+#define OPCODE_SUBS     0x758  // SUBS (register)
+#define OPCODE_SUBIS_0  0x788  // SUBIS (immediate). SHIFT=000
+#define OPCODE_SUBIS_1  0x78A  // SUBIS (immediate). SHIFT=010
+#define OPCODE_CMP      0x759  // CMP (register)
+// #define OPCODE_CMPI     0xF1   // CMP (immediate) ES IGUAL A SUBIS
+#define OPCODE_ANDS     0x750  // ANDS (shifted register)
+#define OPCODE_EOR      0x650  // EOR (shifted register)
+#define OPCODE_ORR      0x550  // ORR (shifted register)
+// #define OPCODE_B        0x5    // B. REVISAR
+// #define OPCODE_BR       0x___ // BR. REVISAR
+// #define OPCODE_B.COND   0x54   // B.COND. REVISAR
+#define OPCODE_LSL      0x34D  // LSL (immediate)
+// #define OPCODE_LSR      0x34D  // LSR (immediate) ES IGUAL A LSL. REVISAR
+#define OPCODE_STUR     0x7C0  // STUR
+#define OPCODE_STURB    0x1C0  // STURB
+#define OPCODE_STURH    0x3C0  // STURH
+#define OPCODE_LDUR     0x7C2  // LDUR
+#define OPCODE_LDURB    0x1C2  // LDURB
+#define OPCODE_LDURH    0x3C2  // LDURH
+#define OPCODE_MOVZ     0x694  // MOVZ
+#define OPCODE_ADD      0x458  // ADD (register)
+#define OPCODE_ADDI_0   0x488  // ADD (immediate). SHIFT=000
+#define OPCODE_ADDI_1   0x48A  // ADD (immediate). SHIFT=010
+#define OPCODE_MUL      0x4D8  // MUL
+#define OPCODE_CBZ      0xB4   // CBZ
+#define OPCODE_CBNZ     0xB5   // CBNZ
 
 // --- Prototipos de funciones auxiliares ---
-
-// Aritméticas
-void execute_add(uint32_t instruction);
-
+void execute_hlt(uint32_t instruction);
 void execute_adds(uint32_t instruction);
-void execute_addis(uint32_t instruction);
+void execute_addis_0(uint32_t instruction);
+void execute_addis_1(uint32_t instruction);
+void execute_subs(uint32_t instruction);
+void execute_subis_0(uint32_t instruction);
+void execute_subis_1(uint32_t instruction);
+void execute_cmp(uint32_t instruction);
+// void execute_cmpi(uint32_t instruction);
+void execute_ands(uint32_t instruction);
+void execute_eor(uint32_t instruction);
+void execute_orr(uint32_t instruction);
+// void execute_b(uint32_t instruction);
+// void execute_br(uint32_t instruction);
+// void execute_b_cond(uint32_t instruction);
+void execute_lsl(uint32_t instruction);
+// void execute_lsr(uint32_t instruction);
+void execute_stur(uint32_t instruction);
+void execute_sturb(uint32_t instruction);
+void execute_sturh(uint32_t instruction);
+void execute_ldur(uint32_t instruction);
+void execute_ldurb(uint32_t instruction);
+void execute_ldurh(uint32_t instruction);
+void execute_movz(uint32_t instruction);
+void execute_add(uint32_t instruction);
+void execute_addi_0(uint32_t instruction);
+void execute_addi_1(uint32_t instruction);
+void execute_mul(uint32_t instruction);
+void execute_cbz(uint32_t instruction);
+void execute_cbnz(uint32_t instruction);
+
 
 void process_instruction()
 {
     uint32_t instruction = mem_read_32(CURRENT_STATE.PC);
-
     printf("Instrucción: 0x%x\n", instruction);
 
     // Defino la recuperación de bits para diferentes tamaños de opcode
-    uint32_t opcode11 = (instruction >> 21) & 0x7FF;
-    printf("Opcode: 0x%x\n", opcode11);
-    NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+    uint32_t opcode_6 = (instruction >> 26) & 0x3F;
+    uint32_t opcode_8 = (instruction >> 24) & 0x7F;
+    uint32_t opcode_11 = (instruction >> 21) & 0x7FF;
+    printf("Opcode 6 bits: 0x%x\n", opcode_6);
+    printf("Opcode 8 bits: 0x%x\n", opcode_8);
+    printf("Opcode 11 bits: 0x%x\n", opcode_11);
 
-    switch(opcode11) {
-        case OPCODE_ADD:
-            printf("ADD\n");
-            execute_add(instruction);
-            break;
-        case OPCODE_ADDS:
-            printf("ADDS\n");
-            execute_adds(instruction);
-            break;
-        case OPCODE_ADDIS:
-            printf("ADDIS\n");
-            execute_addis(instruction);
-            break;
-        default:
-            printf("Instrucción no implementada: 0x%x (opcode 0x%x)\n", instruction, opcode11);
-            break;
-    }
+    NEXT_STATE.PC = CURRENT_STATE.PC + 4;
 }
 
 void execute_add(uint32_t instruction)
