2360b38270950dd08b14ee20150882851a715212
timomenceyra <tmenceyra@udesa.edu.ar>
2025-03-30 23:57:46 -0300

    Final

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -3,6 +3,34 @@
 #include <string.h>
 #include "shell.h"
 
+// Definición de las funciones de ejecución de instrucciones
+void execute_hlt(uint32_t instruction);
+void execute_adds(uint32_t instruction);
+void execute_addis_0(uint32_t instruction);
+void execute_addis_1(uint32_t instruction);
+void execute_subs(uint32_t instruction);
+void execute_subis_0(uint32_t instruction);
+void execute_subis_1(uint32_t instruction);
+void execute_ands(uint32_t instruction);
+void execute_eor(uint32_t instruction);
+void execute_orr(uint32_t instruction);
+void execute_b(uint32_t instruction);
+void execute_br(uint32_t instruction);
+void execute_b_cond(uint32_t instruction);
+void execute_lsl(uint32_t instruction);
+void execute_lsr(uint32_t instruction);
+void execute_stur(uint32_t instruction);
+void execute_sturb(uint32_t instruction);
+void execute_sturh(uint32_t instruction);
+void execute_ldur(uint32_t instruction);
+void execute_ldurb(uint32_t instruction);
+void execute_ldurh(uint32_t instruction);
+void execute_movz(uint32_t instruction);
+void execute_add(uint32_t instruction);
+void execute_mul(uint32_t instruction);
+void execute_cbz(uint32_t instruction);
+void execute_cbnz(uint32_t instruction);
+
 // OPCODES
 #define OPCODE_HLT      0x6A2  // HLT
 #define OPCODE_ADDS     0x558  // ADDS (register)
@@ -156,11 +184,7 @@ void process_instruction()
 }
 
 
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
-#include "shell.h"
-
+// Implementación de las funciones de ejecución de instrucciones
 void execute_hlt(uint32_t instruction)
 {
     printf("HLT\n");
