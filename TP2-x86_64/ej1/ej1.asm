b9d1e396a51e64166dbeb2319f06a38373197bc5
Peter <127430417+Pedro-Lonardi@users.noreply.github.com>
2025-04-16 16:20:06 -0300

    a ver si más simple es mejor

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -19,10 +19,81 @@ extern str_concat
 
 
 string_proc_list_create_asm:
+    push rbp
+    mov rbp, rsp
+    mov edi, 16
+    call malloc
+    test rax, rax
+    jz .Ldone
+    mov qword [rax], 0
+    mov qword [rax+8], 0
+.Ldone:
+    pop rbp
+    ret
 
 string_proc_node_create_asm:
+    push rbp
+    mov rbp, rsp
+    mov edi, 32
+    call malloc
+    test rax, rax
+    jz .Lend
+    mov qword [rax], 0
+    mov qword [rax+8], 0
+    mov byte [rax+16], dil
+    mov qword [rax+24], rsi
+.Lend:
+    pop rbp
+    ret
 
 string_proc_list_add_node_asm:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 8
+    mov edi, esi
+    mov rsi, rdx
+    call string_proc_node_create_asm
+    add rsp, 8
+    test rax, rax
+    jz .Ldone
+    mov rcx, [rdi+8]
+    test rcx, rcx
+    jnz .Lnon_empty
+    mov [rdi], rax
+    mov [rdi+8], rax
+    jmp .Ldone
+.Lnon_empty:
+    mov [rcx], rax
+    mov [rax+8], rcx
+    mov [rdi+8], rax
+.Ldone:
+    pop rbp
+    ret
 
 string_proc_list_concat_asm:
+    push rbp
+    mov rbp, rsp
+    push rbx
+    sub rsp, 8
+    mov ebx, esi
+    mov rcx, rdx
+    mov rax, [rdi]
+.Lloop:
+    test rax, rax
+    je .Ldone
+    cmp byte [rax+16], bl
+    jne .Lnext
+    mov rdi, rcx
+    mov rsi, [rax+24]
+    call str_concat
+    mov rcx, rax
+.Lnext:
+    mov rax, [rax]
+    jmp .Lloop
+.Ldone:
+    add rsp, 8
+    pop rbx
+    pop rbp
+    mov rax, rcx
+    ret
 
