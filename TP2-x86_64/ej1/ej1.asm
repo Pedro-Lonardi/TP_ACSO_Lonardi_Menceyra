80dc12de0c60fdaba2250f30b81f89cd3116f1ce
Peter <127430417+Pedro-Lonardi@users.noreply.github.com>
2025-04-16 01:38:34 -0300

    asm done?

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -13,25 +13,26 @@ global string_proc_list_add_node_asm
 global string_proc_list_concat_asm
 
 ; FUNCIONES auxiliares que pueden llegar a necesitar:
+extern strdup
 extern malloc
 extern free
 extern str_concat
 
 
 string_proc_list_create_asm:
-push rbp
-mov rbp, rsp
+    push rbp
+    mov rbp, rsp
 
-mov edi, 16
-call malloc
+    mov edi, 16
+    call malloc
 
-test rax, rax
-je .return_null
+    test rax, rax
+    je .return_null
 
-mov qword [rax], 0
-mov qword [rax + 8], 0
+    mov qword [rax], 0
+    mov qword [rax + 8], 0
 
-jmp .done
+    jmp .done
 
 .return_null:
 
@@ -40,8 +41,104 @@ pop rbp
 ret
 
 string_proc_node_create_asm:
+    push rbp
+    mov rbp, rsp
+
+    mov edi, 32
+    call malloc
+
+    test rax, rax
+    je .return_null
+
+    mov qword [rax], 0
+
+    mov qword [rax + 8], 0
+
+    mov byte [rax + 16], dil
+
+    mov [rax + 24], rsi
+
+    jmp .done
+
+.return_null:
+
+.done:
+    pop rbp
+    ret
 
 string_proc_list_add_node_asm:
+    push rbp
+    mov rbp, rsp
+
+    mov r8, rdi
+    mov r9b, sil
+    mov r10, rdx
+
+    mov dil, r9b
+    mov rsi, r10
+    call string_proc_node_create_asm
+
+    test rax, rax
+    je .end
+
+    cmp qword [r8], 0
+    je .lista_vacia
+
+    mov rcx, [r8 + 8]
+    mov [rcx], rax
+
+    mov [rax + 8], rcx
+
+    mov [r8 + 8], rax
+
+    jmp .end
+
+.lista_vacia:
+    mov [r8], rax
+    mov [r8 + 8], rax
+
+.end:
+    pop rbp
+    ret
 
 string_proc_list_concat_asm:
+    push rbp
+    mov rbp, rsp
 
+    mov r8, rdi
+    mov r9b, sil
+    mov r10, rdx
+
+    mov rdi, r10
+    call strdup
+    mov r12, rax
+
+    mov r11, [r8]
+
+.loop:
+    test r11, r11
+    je .done
+
+    mov al, [r11 + 16]
+    cmp al, r9b
+    jne .next_node
+
+    mov rdi, r12
+    mov rsi, [r11 + 24]
+    call str_concat
+    mov r13, rax
+
+    mov rdi, r12
+    call free
+
+    mov r12, r13
+
+.next_node:
+    mov r11, [r11]
+    jmp .loop
+
+.done:
+    mov rax, r12 
+
+    pop rbp
+    ret
