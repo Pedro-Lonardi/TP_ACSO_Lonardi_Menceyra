4f6fed3ec6f49348c2e089f5163507f497af4550
Peter <127430417+Pedro-Lonardi@users.noreply.github.com>
2025-04-16 16:25:09 -0300

    ojala

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -12,61 +12,68 @@ global string_proc_node_create_asm
 global string_proc_list_add_node_asm
 global string_proc_list_concat_asm
 
-; FUNCIONES auxiliares que pueden llegar a necesitar:
 extern malloc
-extern free
+extern string_proc_node_create_asm
 extern str_concat
-
 
 string_proc_list_create_asm:
     push rbp
     mov rbp, rsp
+    sub rsp, 8
     mov edi, 16
     call malloc
+    add rsp, 8
     test rax, rax
-    jz .Ldone
+    jz .L1
     mov qword [rax], 0
     mov qword [rax+8], 0
-.Ldone:
+.L1:
     pop rbp
     ret
 
 string_proc_node_create_asm:
     push rbp
     mov rbp, rsp
+    sub rsp, 8
     mov edi, 32
     call malloc
+    add rsp, 8
     test rax, rax
-    jz .Lend
+    jz .L2
     mov qword [rax], 0
     mov qword [rax+8], 0
     mov byte [rax+16], dil
     mov qword [rax+24], rsi
-.Lend:
+.L2:
     pop rbp
     ret
 
 string_proc_list_add_node_asm:
     push rbp
     mov rbp, rsp
+    push rbx
+    push r12
     sub rsp, 8
+    mov r12, rdi
     mov edi, esi
     mov rsi, rdx
     call string_proc_node_create_asm
     add rsp, 8
     test rax, rax
-    jz .Ldone
-    mov rcx, [rdi+8]
-    test rcx, rcx
-    jnz .Lnon_empty
-    mov [rdi], rax
-    mov [rdi+8], rax
-    jmp .Ldone
-.Lnon_empty:
-    mov [rcx], rax
-    mov [rax+8], rcx
-    mov [rdi+8], rax
-.Ldone:
+    jz .L3
+    mov rbx, [r12+8]
+    test rbx, rbx
+    jnz .L4
+    mov [r12], rax
+    mov [r12+8], rax
+    jmp .L3
+.L4:
+    mov [rbx], rax
+    mov [rax+8], rbx
+    mov [r12+8], rax
+.L3:
+    pop r12
+    pop rbx
     pop rbp
     ret
 
@@ -74,26 +81,27 @@ string_proc_list_concat_asm:
     push rbp
     mov rbp, rsp
     push rbx
+    push r12
     sub rsp, 8
-    mov ebx, esi
+    mov bl, sil
     mov rcx, rdx
-    mov rax, [rdi]
-.Lloop:
-    test rax, rax
-    je .Ldone
-    cmp byte [rax+16], bl
-    jne .Lnext
+    mov r12, [rdi]
+.L5:
+    test r12, r12
+    je .L6
+    cmp bl, byte [r12+16]
+    jne .L7
     mov rdi, rcx
-    mov rsi, [rax+24]
+    mov rsi, [r12+24]
     call str_concat
     mov rcx, rax
-.Lnext:
-    mov rax, [rax]
-    jmp .Lloop
-.Ldone:
+.L7:
+    mov r12, [r12]
+    jmp .L5
+.L6:
     add rsp, 8
+    pop r12
     pop rbx
     pop rbp
     mov rax, rcx
     ret
-
