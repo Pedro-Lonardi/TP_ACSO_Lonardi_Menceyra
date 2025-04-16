50630b4eefe51cc2bc534dec74daf2a169e4c1bd
Peter <127430417+Pedro-Lonardi@users.noreply.github.com>
2025-04-16 14:54:05 -0300

    chat ?

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -1,162 +1,134 @@
-    .intel_syntax noprefix
-    .section .text
+; /** defines bool y puntero **/
+%define NULL 0
+%define TRUE 1
+%define FALSE 0
 
-    .extern malloc
-    .extern free
-    .extern fprintf
-    .extern strdup
-    .extern str_concat
-    .extern stderr
+section .data
 
-############################################
-# string_proc_list_create_asm
-############################################
+section .text
+
+.intel_syntax noprefix
+.section .text
 .global string_proc_list_create_asm
-.type string_proc_list_create_asm, @function
-string_proc_list_create_asm:
-    mov     rdi, 16                  # sizeof(string_proc_list)
-    call    malloc
-    test    rax, rax
-    je      .error_list_create
-    mov     qword ptr [rax], 0      # list->first = NULL
-    mov     qword ptr [rax + 8], 0  # list->last  = NULL
-    ret
-
-.error_list_create:
-    mov     rdi, qword ptr stderr[rip]
-    mov     rsi, offset .errmsg_list
-    call    fprintf
-    xor     rax, rax
-    ret
-
-############################################
-# string_proc_node_create_asm
-############################################
 .global string_proc_node_create_asm
-.type string_proc_node_create_asm, @function
-string_proc_node_create_asm:
-    push    rbp
-    mov     rbp, rsp
-
-    mov     rdi, 24                  # sizeof(string_proc_node)
-    call    malloc
-    test    rax, rax
-    je      .error_node_create
-
-    mov     byte ptr [rax + 16], dil    # node->type
-    mov     qword ptr [rax + 8], rsi    # node->hash
-    mov     qword ptr [rax], 0          # node->next
-    mov     qword ptr [rax + 16 + 8], 0 # node->previous
-
-    leave
-    ret
-
-.error_node_create:
-    mov     rdi, qword ptr stderr[rip]
-    mov     rsi, offset .errmsg_node
-    call    fprintf
-    xor     rax, rax
-    leave
-    ret
-
-############################################
-# string_proc_list_add_node_asm
-############################################
 .global string_proc_list_add_node_asm
-.type string_proc_list_add_node_asm, @function
-string_proc_list_add_node_asm:
-    push    rbp
-    mov     rbp, rsp
-    push    rbx
-
-    mov     rbx, rdi                # list
-    mov     dil, sil               # type (1st arg)
-    mov     rsi, rdx               # hash (2nd arg)
-    call    string_proc_node_create_asm
-
-    test    rax, rax
-    je      .error_add_node
-
-    mov     rcx, qword ptr [rbx]   # list->first
-    test    rcx, rcx
-    je      .empty_list
-
-    mov     rdx, qword ptr [rbx + 8]  # list->last
-    mov     qword ptr [rdx], rax      # last->next = node
-    mov     qword ptr [rax + 16], rdx # node->previous = last
-    mov     qword ptr [rbx + 8], rax  # list->last = node
-    jmp     .done_add
-
-.empty_list:
-    mov     qword ptr [rbx], rax      # list->first = node
-    mov     qword ptr [rbx + 8], rax  # list->last  = node
-    jmp     .done_add
-
-.error_add_node:
-    mov     rdi, qword ptr stderr[rip]
-    mov     rsi, offset .errmsg_node
-    call    fprintf
-
-.done_add:
-    pop     rbx
-    leave
-    ret
-
-############################################
-# string_proc_list_concat_asm
-############################################
 .global string_proc_list_concat_asm
-.type string_proc_list_concat_asm, @function
-string_proc_list_concat_asm:
-    push    rbp
-    mov     rbp, rsp
-    push    rbx
-    push    r12
-    push    r13
-    push    r14
 
-    mov     rbx, rdi                # list
-    mov     r12b, sil               # type
-    mov     r13, rdx                # hash
+.extern malloc
+.extern free
+.extern fprintf
+.extern strdup
+.extern str_concat
+.extern stderr
 
-    mov     rdi, r13
-    call    strdup
-    mov     r14, rax                # result
-
-    mov     rsi, qword ptr [rbx]    # current_node = list->first
-
-.loop_concat:
-    test    rsi, rsi
-    je      .done_concat
-
-    mov     al, byte ptr [rsi + 16]   # current_node->type
-    cmp     al, r12b
-    jne     .next_node
-
-    mov     rdi, r14
-    mov     rsi, qword ptr [rsi + 8]  # current_node->hash
-    call    str_concat
-    mov     rdi, r14
-    call    free
-    mov     r14, rax
-
-.next_node:
-    mov     rsi, qword ptr [rsi]      # current_node = current_node->next
-    jmp     .loop_concat
-
-.done_concat:
-    mov     rax, r14                  # return result
-
-    pop     r14
-    pop     r13
-    pop     r12
-    pop     rbx
-    leave
+;----------------------------------------------------------------------
+; string_proc_list_create_asm(void)
+;   - Reserva 16 bytes para el struct string_proc_list.
+;   - Si falla la asignación, imprime "Error: No se pudo crear la lista\n"
+;     y retorna NULL.
+;   - Inicializa list->first y list->last a 0.
+;----------------------------------------------------------------------
+string_proc_list_create_asm:
+    mov rdi, 16
+    call malloc
+    test rax, rax
+    jne .create_list_ok
+    mov rdi, qword ptr [stderr]
+    lea rsi, [rip+err_list]
+    xor rax, rax
+    call fprintf
+    xor rax, rax
+    ret
+.create_list_ok:
+    mov qword ptr [rax], 0
+    mov qword ptr [rax+8], 0
     ret
 
-############################################
+;----------------------------------------------------------------------
+; string_proc_node_create_asm(uint8_t type, char* hash)
+;----------------------------------------------------------------------
+string_proc_node_create_asm:
+    mov r8b, dil
+    mov rdi, 32
+    call malloc
+    test rax, rax
+    jne .create_node_ok
+    mov rdi, qword ptr [stderr]
+    lea rsi, [rip+err_node]
+    xor rax, rax
+    call fprintf
+    xor rax, rax
+    ret
+.create_node_ok:
+    mov qword ptr [rax], 0
+    mov qword ptr [rax+8], 0
+    mov byte ptr [rax+16], r8b
+    mov qword ptr [rax+24], rsi
+    ret
+
+;----------------------------------------------------------------------
+; string_proc_list_add_node_asm(string_proc_list* list, uint8_t type, char* hash)
+;----------------------------------------------------------------------
+string_proc_list_add_node_asm:
+    mov r8, rdi
+    mov rdi, rsi
+    mov rsi, rdx
+    call string_proc_node_create_asm
+    test rax, rax
+    je .node_fail
+    mov rcx, qword ptr [r8]
+    test rcx, rcx
+    jne .list_not_empty
+    mov qword ptr [r8], rax
+    mov qword ptr [r8+8], rax
+    jmp .add_node_end
+.list_not_empty:
+    mov rcx, qword ptr [r8+8]
+    mov qword ptr [rcx], rax
+    mov qword ptr [rax+8], rcx
+    mov qword ptr [r8+8], rax
+.add_node_end:
+    ret
+.node_fail:
+    ret
+
+;----------------------------------------------------------------------
+; string_proc_list_concat_asm(string_proc_list* list, uint8_t type, char* hash)
+;----------------------------------------------------------------------
+string_proc_list_concat_asm:
+    push rbx
+    push r12
+    push r13
+    mov r12, rdi
+    mov r13b, sil
+    mov rdi, rdx
+    call strdup
+    mov rbx, rax
+    mov rcx, qword ptr [r12]
+.concat_loop:
+    cmp rcx, 0
+    je .concat_done
+    mov al, byte ptr [rcx+16]
+    cmp al, r13b
+    jne .skip_concat
+    mov rdi, rbx
+    mov rsi, qword ptr [rcx+24]
+    call str_concat
+    mov rdi, rbx
+    call free
+    mov rbx, rax
+.skip_concat:
+    mov rcx, qword ptr [rcx]
+    jmp .concat_loop
+.concat_done:
+    mov rax, rbx
+    pop r13
+    pop r12
+    pop rbx
+    ret
+
 .section .rodata
-.errmsg_list:
+err_list:
     .string "Error: No se pudo crear la lista\n"
-.errmsg_node:
+err_node:
     .string "Error: No se pudo crear el nodo\n"
