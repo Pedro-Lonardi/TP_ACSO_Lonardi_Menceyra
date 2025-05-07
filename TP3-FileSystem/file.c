f694f1fd656c0b1a53a7b6c11ea495060628fade
Peter <127430417+Pedro-Lonardi@users.noreply.github.com>
2025-05-07 19:35:40 -0300

    inode y file?

--- TP3-FileSystem/file.c
+++ TP3-FileSystem/file.c
@@ -9,7 +9,30 @@
  * TODO
  */
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
-    
-    return 0;
+    struct inode in;
+    if (inode_iget(fs, inumber, &in) < 0) {
+        return -1;
+    }
+
+    int dataBlockNum = inode_indexlookup(fs, &in, blockNum);
+    if (dataBlockNum == -1) {
+        return -1;
+    }
+
+    if (diskimg_readblock(fs->dfd, dataBlockNum, buf) < 0) {
+        return -1;
+    }
+
+    int fileSize = inode_getsize(&in);
+    int totalFullBlocks = fileSize / BLOCK_SIZE;
+    int remainder = fileSize % BLOCK_SIZE;
+
+    if (blockNum < totalFullBlocks) {
+        return BLOCK_SIZE;
+    } else if (blockNum == totalFullBlocks) {
+        return remainder;
+    } else {
+        return 0;
+    }
 }
 
--- TP3-FileSystem/inode.c
+++ TP3-FileSystem/inode.c
@@ -10,16 +10,73 @@
  */
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
     //Implement Code Here
-    return 0; 
+    if (inumber < 1 || inumber > fs->superblock.s_isize * INODES_PER_BLOCK) {
+        return -1;
+    }
+
+    int inumber0 = inumber - 1;
+    int blockNum = inumber0 / INODES_PER_BLOCK;
+    int offset = inumber0 % INODES_PER_BLOCK;
+
+    char buffer[BLOCK_SIZE];
+    if (diskimg_readblock(fs->dfd, INODE_START_SECTOR + blockNum, buffer) < 0) {
+        return -1;
+    }
+
+    struct inode *inodeBlock = (struct inode *)buffer;
+    *inp = inodeBlock[offset];
+
+    return 0;
 }
 
 /**
  * TODO
  */
-int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp,
-    int blockNum) {  
-        //Implement code here
-    return 0;
+int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {  
+    //Implement code here
+    if ((inp->i_mode & IALLOC) == 0) {
+        return -1; // inode not allocated
+    }
+
+    if ((inp->i_mode & ILARG) == 0) {
+        // archivo "pequeño": bloques directos
+        if (blockNum < 0 || blockNum >= 8) {
+            return -1;
+        }
+        return inp->i_addr[blockNum];
+    } else {
+        // archivo "grande": bloques indirectos
+        if (blockNum < 0 || blockNum >= 7 * 256 + 256 * 256) {
+            return -1;
+        }
+
+        if (blockNum < 7 * 256) {
+            int indirBlockIndex = blockNum / 256;
+            int indirBlockOffset = blockNum % 256;
+
+            uint16_t indir[256];
+            if (diskimg_readblock(fs->dfd, inp->i_addr[indirBlockIndex], (char *)indir) < 0) {
+                return -1;
+            }
+            return indir[indirBlockOffset];
+        } else {
+            int adjusted = blockNum - 7 * 256;
+            int indir1 = adjusted / 256;
+            int indir2 = adjusted % 256;
+
+            uint16_t firstLevel[256];
+            if (diskimg_readblock(fs->dfd, inp->i_addr[7], (char *)firstLevel) < 0) {
+                return -1;
+            }
+
+            uint16_t secondLevel[256];
+            if (diskimg_readblock(fs->dfd, firstLevel[indir1], (char *)secondLevel) < 0) {
+                return -1;
+            }
+
+            return secondLevel[indir2];
+        }
+    }
 }
 
 int inode_getsize(struct inode *inp) {
