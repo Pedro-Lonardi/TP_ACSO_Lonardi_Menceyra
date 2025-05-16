e81ca69a28c667c31a96baaf070c25fd786fa189
Peter <127430417+Pedro-Lonardi@users.noreply.github.com>
2025-05-16 19:41:20 -0300

    a ver a ver

--- TP4-Shell/src/ej1/ring.c
+++ TP4-Shell/src/ej1/ring.c
@@ -14,7 +14,50 @@ int main(int argc, char **argv)
     
     /* Parsing of arguments */
   	/* TO COMPLETE */
+	n = atoi(argv[1]);
+    buffer[0] = atoi(argv[2]);
+    start = atoi(argv[3]);
     printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer[0], start);
     
    	/* You should start programming from here... */
+
+	int pipes[n][2];
+
+    for (int i = 0; i < n; i++) {
+        if (pipe(pipes[i]) == -1) {
+            perror("pipe");
+            exit(1);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0) {
+            for (int j = 0; j < n; j++) {
+                if (j != i) close(pipes[j][0]);
+                if (j != (i + n - 1) % n) close(pipes[j][1]);
+            }
+
+            int msg;
+            read(pipes[(i + n - 1) % n][0], &msg, sizeof(int));
+            msg++;
+            write(pipes[i][1], &msg, sizeof(int));
+
+            exit(0);
+        }
+    }
+
+    write(pipes[(start + n - 1) % n][1], buffer, sizeof(int));
+
+    read(pipes[(start + n - 2) % n][0], buffer, sizeof(int));
+    printf("Resultado final: %d\n", buffer[0]);
+
+    for (int i = 0; i < n; i++) {
+        wait(NULL);
+    }
+
+    return 0;
 }
